ea00f70e4d71180396694c9862e82a254834374d
Tebogo Lejaka <tklejaka@gmail.com>
2020-08-21 15:16:44 +0200

    Demostrating updating if object data exists and insert into the data source if not

--- routes/api/municipalities.js
+++ routes/api/municipalities.js
@@ -43,4 +43,34 @@ router.post("/", (req, res) => {
     res.json(municipalities);
 });
 
+//Updating an Object 
+router.put("/", (req, res) => {
+    const existingMunicipality = {
+        id: uuid.v4(),
+        name: req.body.name,
+        province: req.body.province
+    };
+    if(!existingMunicipality.name || !existingMunicipality.province){
+       return res.status(400).json({
+            msg: "Please include the name of the municipality and the province"
+        });
+    }
+
+    const found = municipalities.some(municipalities => municipalities.name == parseInt(req.params.name));
+    if(found==true){
+        for(municipality of municipalities){
+            if(existingMunicipality.name == municipality.name){
+                console.log(`This is the municipality ${municipality.name} to update`);
+                municipality.province = existingMunicipality.province;
+                console.log(`The municipality ${municipality.name} changed to \n ${municipality}`);
+            } 
+        }
+    }
+    else{
+        console.log(`The municipality ${existingMunicipality.name} is not found, therefore added`);
+        municipalities.push(existingMunicipality);
+        res.json(municipalities);
+    }
+});
+
 module.exports = router;
