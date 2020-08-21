d2ed1d91f654db32d93f8d49b3e018a0c4e1fdc3
Tebogo Lejaka <tklejaka@gmail.com>
2020-08-21 13:18:13 +0200

    Added a rest service endpoint to return json

---
+++ Municipalities.js
@@ -0,0 +1,44 @@
+[
+    {
+        id: 1,
+        name: "Buffalo City Metropolitan Municipality",
+        province: "Eastern Cape"
+    },
+    {
+        id: 2,
+        name: "City of Cape Town Metropolitan Municipality",
+        province: "Western Cape"
+    },
+    {
+        id: 3,
+        name: "City of Johannesburg Metropolitan Municipality",
+        province: "Gauteng"
+    },
+    {
+        id: 4,
+        name: "City of Tshwane Metropolitan Municipality",
+        province: "Gauteng"
+    },
+    {
+        id: 5,
+        name: "Ekurhuleni Metropolitan Municipality",
+        province: "Gauteng"
+    },
+    {
+        id: 6,
+        name: "eThekwini Metropolitan Municipality",
+        province: "KwaZulu-Natal"
+    },
+    {
+        id: 7,
+        name: "Mangaung Metropolitan Municipality",
+        province: "Free State"
+    },
+    {
+        id: 8,
+        name: "Nelson Mandela Bay Metropolitan Municipality",
+        province: "Eastern Cape"
+    }
+];
+
+module.exports = municipalities;
