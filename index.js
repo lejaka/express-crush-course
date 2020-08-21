9e9758577b1deeebfd42ed6cfeae097f2410e55c
Tebogo Lejaka <tklejaka@gmail.com>
2020-08-21 12:42:40 +0200

    Before using an static folder to set routes and loading html file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+//port running on
 const PORT = process.env.PORT || 5000;
+//adding route
+//function vs arrow function
+// app.get('/', function(req,res){
 
+// });
+app.get('/', (req,res) => {
+   // res.send("Hello World from Node Express. :-)");
+    res.sendFile(path.join(__dirname, "public","index.html"));
+});
 app.listen(PORT, () => console.log(`Hello World Express App listening on Port: ${PORT}`));
