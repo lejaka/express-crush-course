68504f005b2086e16ee7f05fe527249f70d4bccc
Tebogo Lejaka <tklejaka@gmail.com>
2020-08-21 14:06:31 +0200

    Return a default status error

--- index.js
+++ index.js
@@ -24,8 +24,24 @@ app.use(logger);
 app.use(express.static(path.join(__dirname, "public")));
 
 //Get all municipalities
-app.get('/api/municipalities', function(req,res){
+app.get("/api/municipalities", function(req,res){
     res.json(municipalities);
 });
+
+//Get a single municipality by ID
+app.get("/api/municipalities/:id", (req, res) => {
+    //Sending the parameters from the request as a response
+    //res.send(req.params.id);
+    //Some returns true or false in order to check if the user with ID exists or not 
+    const found = municipalities.some(municipalities => municipalities.id ===  parseInt(req.params.id));
+    if(found==true){
+        res.json(municipalities.filter(municipalities => municipalities.id === parseInt(req.params.id)));
+    }
+    else{
+        res.status(400).json({
+            message: `Municipality with that ID was not found ${moment().format()}`
+        });
+    }
+})
 
 app.listen(PORT, () => console.log(`Hello World Express App listening on Port: ${PORT}`));
